8f8a2237f61d5e97d84ea2640e7f4454d0018778
nataemi <38769153+nataemi@users.noreply.github.com>
2018-10-12 00:58:26 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,1 @@
-{\rtf1\ansi\ansicpg1250\cocoartf1561\cocoasubrtf200
-{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red28\green0\blue207;}
-{\*\expandedcolortbl;;\csgenericrgb\c0\c0\c0;\csgenericrgb\c11000\c0\c81000;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab543
-\pard\tx543\pardeftab543\pardirnatural\partightenfactor0
-
-\f0\fs22 \cf2 \CocoaLigature0 java -jar target/rekrutacyjne\cf3 -0.0.1\cf2 -SNAPSHOT-jar-with-dependencies.jar $*}
+java -jar target/rekrutacyjne\cf3 -0.0.1\cf2 -SNAPSHOT-jar-with-dependencies.jar $*}
